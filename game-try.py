586e5348e4c30de41962963a17c923b7ff8672e3
daffadae <dafadae@gmail.com>
2023-02-21 19:12:43 +0200

    kolmas tund punane ruut

--- game-try.py
+++ game-try.py
@@ -1,2 +1,25 @@
 import pygame as pg
 
+pg.init()
+
+window = pg.display.set_mode((800,400))
+gameplay = True
+
+class rectangle:
+    def __init__(self, sideSize):
+        self.sideSize = sideSize
+
+xKordinaat = 1
+
+while gameplay:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            gameplay = False
+            break
+    if xKordinaat==799:
+        xKordinaat = 0
+    pg.time.delay(5)
+    window.fill((0,0,0))
+    pg.draw.rect(window, (255, 0, 0), (xKordinaat,30,60,60)) #(window, (*rgb colour*), (sideSize1,sideSize2,x,y))
+    pg.display.update()
+    xKordinaat += 1
