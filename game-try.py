500f08e88c785cca8edbe0c2879ddf951871d6a8
daffadae <dafadae@gmail.com>
2023-03-14 19:12:15 +0200

    kolmas tund punane ruut

--- game-try.py
+++ game-try.py
@@ -2,24 +2,26 @@ import pygame as pg
 
 pg.init()
 
-window = pg.display.set_mode((800,400))
-gameplay = True
+window = pg.display.set_mode((1280,720))
+pg.display.set_caption('Dating sim!')
+running = True
 
-class rectangle:
-    def __init__(self, sideSize):
-        self.sideSize = sideSize
+icon = pg.image.load('pics/icon.png')
+pg.display.set_icon(icon)
 
-xKordinaat = 1
 
-while gameplay:
+def character():
+    x = 0
+    y = 0
+    charImg = pg.image.load("pics/character.png")
+    window.blit(charImg, (x, y))
+
+while running:
+    window.fill((255, 255, 255))
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            gameplay = False
+            running = False
             break
-    if xKordinaat==799:
-        xKordinaat = 0
-    pg.time.delay(5)
-    window.fill((0,0,0))
-    pg.draw.rect(window, (255, 0, 0), (xKordinaat,30,60,60)) #(window, (*rgb colour*), (sideSize1,sideSize2,x,y))
+    #    if event.type == pg.KEYDOWN:
+    #        if event.key == pg.K_SPACE:
     pg.display.update()
-    xKordinaat += 1
