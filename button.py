2b160bb98d66253a2e7154fab557687fb6df2a5a
daffadae <dafadae@gmail.com>
2023-04-11 18:31:07 +0300

    proving to understand

--- button.py
+++ button.py
@@ -1,10 +1,13 @@
 import pygame as pg
 import sys
+import fromTheStart as main
 
 pg.init()
 window = pg.display.set_mode((1280, 720))
 pg.display.set_caption("Date Sim!")
 main_font = pg.font.SysFont(None, 50)
+def switch():
+    print("worked")
 class Button():
     def __init__(self, image, x_pos, y_pos, text_input):
         self.image = image
@@ -21,29 +24,9 @@ class Button():
 
     def checkForInput(self, position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-            print("It worked!!!")
+            switch()
     def changeColor(self, position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
             self.text = main_font.render(self.text_input, True, (127,255,0))
         else:
             self.text = main_font.render(self.text_input, True,(255, 255, 255))
-
-button_surface = pg.image.load("button.jpeg")
-button_surface = pg.transform.scale(button_surface, (430, 120))
-
-button = Button(button_surface, 640, 360, "Start!")
-
-while True:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
-        elif event.type == pg.MOUSEBUTTONDOWN:
-            button.checkForInput(pg.mouse.get_pos())
-
-    window.fill((255, 255, 255))
-
-    button.update()
-    button.changeColor(pg.mouse.get_pos())
-
-    pg.display.update()
---
+++ fromTheStart.py
@@ -0,0 +1,55 @@
+import pygame as pg
+import sys
+
+pg.init()
+window = pg.display.set_mode((1280, 720))
+pg.display.set_caption("Date Sim!")
+main_font = pg.font.SysFont(None, 50)
+def switch():
+    pg.image.load_extended(dialogue1[0])
+class Button():
+    def __init__(self, image, x_pos, y_pos, text_input):
+        self.image = image
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_input = text_input
+        self.text = main_font.render(self.text_input, True, (255, 255, 255))
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+    def update(self):
+        window.blit(self.image, self.rect)
+        window.blit(self.text, self.text_rect)
+
+    def checkForInput(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            switch()
+    def changeColor(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            self.text = main_font.render(self.text_input, True, (127,255,0))
+        else:
+            self.text = main_font.render(self.text_input, True,(255, 255, 255))
+
+
+button_surface = pg.image.load("button.jpeg")
+button_surface = pg.transform.scale(button_surface, (430, 120))
+
+button = Button(button_surface, 640, 360, "Start!")
+
+while True:
+    for event in pg.event.get():
+        window.fill((255, 255, 255))
+
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            button.checkForInput(pg.mouse.get_pos())
+
+
+    button.update()
+    button.changeColor(pg.mouse.get_pos())
+
+    pg.display.update()
+
+    dialogue1 = pg.image.load(['pics/1.png','pics/2.png','pics/3.png'])
